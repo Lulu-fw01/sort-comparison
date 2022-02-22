4dcf38d91dbd20b9c79a46c3c0537f57faa78bc8
luka <limarkov@edu.hse.ru>
2022-02-22 17:28:43 +0300

    heapSort started

---
+++ sort-comparison/include/Lulu/Sorts/HeapSort.h
@@ -0,0 +1,16 @@
+//
+// Created by luka on 22.02.2022.
+//
+
+#ifndef SORT_COMPARISON_HEAPSORT_H
+#define SORT_COMPARISON_HEAPSORT_H
+
+#include <vector>
+
+namespace Lulu::Sorts {
+
+    void heapSort(std::vector<int>&);
+
+}
+
+#endif //SORT_COMPARISON_HEAPSORT_H
---
+++ sort-comparison/lib/HeapSort.cpp
@@ -0,0 +1,9 @@
+//
+// Created by luka on 22.02.2022.
+//
+
+#include "Lulu/Sorts/HeapSort.h"
+
+void Lulu::Sorts::heapSort(std::vector<int>& array) {
+
+}
