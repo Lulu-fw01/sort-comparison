3ced4d1d578e67b79c3be05d3d691a3ce7fe223f
luka <limarkov@edu.hse.ru>
2022-02-25 19:58:14 +0300

    funcs sort codestyle

--- sort-comparison/include/Lulu/Utils/Funcs.h
+++ sort-comparison/include/Lulu/Utils/Funcs.h
@@ -9,19 +9,19 @@
 
 namespace Lulu::Utils {
 
-    int getMax(const std::vector<int>&);
+    int getMax(const std::vector<int> &arr);
 
-    int getDigitBy256(int, int);
+    int getDigitBy256(int num, int digitNum);
 
-    bool isSorted(const std::vector<int>&);
+    bool isSorted(const std::vector<int> &arr);
 
-    void printVector(const std::vector<int>&);
+    void printVector(const std::vector<int> &vec);
 
-    int getRandomNum(int, int);
+    int getRandomNum(int min, int max);
 
-    std::vector<int> getRandomVector(int);
+    std::vector<int> getRandomVector(int length);
 
-    std::vector<int> getRandomVector(int, int, int);
+    std::vector<int> getRandomVector(int length, int min, int max);
 }
 
 #endif //SORT_COMPARISON_GETDIGIT_H
